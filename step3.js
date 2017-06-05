37713aebaeb4802748f71368deb6ff672c8f09e3
caitlinquon <caitlin_quon@hotmail.com>
2017-06-05 20:31:37 +0000

    printing with parameters

---
+++ step3.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+function getAndPrintHTML(options) {
+  var buffer = "";
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      buffer += data;
+    });
+    response.on('end', function () {
+      console.log(buffer);
+    });
+  });
+}
+
+getAndPrintHTML(requestOptions);
+
