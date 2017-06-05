1e8943ae3b3850a1dd41326293323241e9187eb9
caitlinquon <caitlin_quon@hotmail.com>
2017-06-05 21:57:17 +0000

    changed the path for reverse and lowercase

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,11 +2,11 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/uppercase.html'
+  path: '/http-examples/step6/reverse.html'
 };
 
 function printReverse(html) {
-  console.log(html.split('').reverse().join(''));
+  console.log(html.split('').reverse().join('ls'));
 
 }
 
